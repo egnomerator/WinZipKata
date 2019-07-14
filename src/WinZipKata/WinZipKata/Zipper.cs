e41f08008f6ebe06eba98db4e85220a963d7b6cf
egnomerator <egnomerator@users.noreply.github.com>
2019-07-14 17:05:52 -0500

    Made first test pass

--- src/WinZipKata/WinZipKata/Zipper.cs
+++ src/WinZipKata/WinZipKata/Zipper.cs
@@ -1,6 +1,12 @@
-﻿namespace WinZipKata
+﻿using System;
+
+namespace WinZipKata
 {
     public static class Zipper
     {
+        public static bool ValidateParentPath(string parentPath)
+        {
+            return true;
+        }
     }
 }
